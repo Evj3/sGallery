0f2f086cbf05e35573ea948ac2de4ea6716b80b8
y0lych <vigilante.volko@gmail.com>
2017-12-04 15:54:23 +1000

    first commit
    add files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var btnPrev = document.querySelector('#gallery .buttons .prev');
+var btnNext = document.querySelector('#gallery .buttons .next');
+
+var images = document.querySelectorAll('#gallery .photos img');
+var i = 0;
+
+btnPrev.onclick = function () {
+    images[i].style.display = 'none';
+    --i;
+    if (i < 0){
+        i = images.length - 1;
+    }
+    images[i].style.display = 'block';
+};
+
+btnNext.onclick = function () {
+    images[i].style.display = 'none';
+    ++i;
+    if (i >= images.length){
+        i = 0;
+    }
+    images[i].style.display = 'block';
+};
